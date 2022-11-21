00d089747dab3254d2abf017f3b746c3d5d657ef
Hank Jackson <hjackson277@gmail.com>
2022-11-21 15:35:51 -0500

    cont hw week 3

--- 61a/homework_week3.scm
+++ 61a/homework_week3.scm
@@ -2,7 +2,7 @@
 
 ;; 1
 
-;; (a, 1.16
+;; (a. 1.16
 
 (define (square x) (* x x))
 
@@ -16,3 +16,71 @@
                       (- n 1)
                       (* a b)))))
   (iter b n 1))
+
+;; (a. opt 1.17
+
+(define (double x) (+ x x))
+(define (half x) (/ x 2))
+
+(define (fast-mult x n)
+  (cond ((= n 1) x)
+        ((even? n) (double (fast-mult x (half n))))
+        (else (+ x (fast-mult x (- n 1))))))
+
+;; (a opt 1.18
+
+(define (fast-mult-iter x n)
+  (define (iter x n prod)
+    (cond ((= n 0) prod)
+          ((even? n) (iter (double x)
+                           (half n)
+                           prod))
+          (else (iter x
+                      (- n 1)
+                      (+ x prod)))))
+  (iter x n 0))
+
+;; (b. 1.35
+
+(define (iterative-improve good-enough? improve)
+  (lambda (guess)
+    (if (good-enough? guess)
+        guess
+        ((iterative-improve good-enough?
+                            improve)(improve guess)))))
+
+(define (fixed-point f first-guess)
+  (define (in-range? guess)
+    (< (abs (- guess (f guess))) 0.00001))
+  ((iterative-improve in-range? f) first-guess))
+
+(fixed-point (lambda (x)(+ 1.0 (/ 1.0 x))) 1.0)
+
+;; (c. 1.37
+
+(define (cont-frac n d k)
+  (if (= k 0)
+      (/ (n k) (d k))
+      (/ (n k)
+         (+ (d k) (cont-frac n d (- k 1))))))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           100)
+
+(define (cont-frac-iter n d k)
+  (define (iter term res)
+    (if (= term 0)
+        res
+        (iter (- term 1)
+              (/ (n term)
+                 (+ (d term) res)))))
+  (iter k 1))
+
+(cont-frac-iter (lambda (i) 1.0)
+                (lambda (i) 1.0)
+                100)
+
+
+
+  
