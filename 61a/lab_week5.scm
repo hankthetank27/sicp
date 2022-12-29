40a925178721106dbc8afa751c91e55b047f6240
Hank Jackson <hjackson277@gmail.com>
2022-12-29 11:19:34 -0500

    started lab week 5

---
+++ 61a/lab_week5.scm
@@ -0,0 +1,46 @@
+#lang simply-scheme
+
+;1.
+
+;;2.25
+(define l1
+  (list 1 3 (list 5 7) 9))
+(car (cdr (car (cdr (cdr l1)))))
+
+(define l2
+  (list (list 7)))
+(car (car l2))
+
+(define l3
+  (list 1(list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+(car
+ (cdr
+  (car
+   (cdr
+    (car
+     (cdr
+      (car
+       (cdr
+        (car
+         (cdr
+          (car
+           (cdr l3)
+           )))))))))))
+
+;;2.53
+(define false #f)
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+;;; (a b c)
+;;; ((george))
+;;; '((y1 y2))
+;;; '(y1 y2)
+;;; false
+;;; false
+;;; '(red shoes blue socks)
+
+
+;;2.27
