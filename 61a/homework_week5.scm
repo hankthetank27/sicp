ab701dfe40d0a784c340fefb54a978d0b1236081
Hank Jackson <hjackson277@gmail.com>
2023-01-14 12:19:29 -0500

    cont week 5 hw

--- 61a/homework_week5.scm
+++ 61a/homework_week5.scm
@@ -3,10 +3,10 @@
 
 ;2.29
 (define (make-mobile left right)
-  (list left right))
+  (cons left right))
 
 (define (make-branch length structure)
-  (list length structure))
+  (cons length structure))
 
 ;a.
 
@@ -14,13 +14,13 @@
   (car mobile))
 
 (define (right-branch mobile)
-  (car (cdr mobile)))
+  (cdr mobile))
 
 (define (branch-length branch)
   (car branch))
 
 (define (branch-structure branch)
-  (car (cdr branch)))
+  (cdr branch))
 
 
 (define mob (make-mobile
@@ -60,5 +60,80 @@
        (mobile-balanced? (branch-structure (left-branch mobile)))
        (mobile-balanced? (branch-structure (right-branch mobile)))))
 
+;2.30
+(define (square-tree-map tree)
+  (map (lambda (el)
+         (if (pair? el)
+             (square-tree el)
+             (* el el)))
+       tree))
 
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((pair? tree)
+         (cons (square-tree (car tree))
+               (square-tree (cdr tree))))
+        (else (* tree tree))))
+                
+         
+(square-tree-map
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+
+;2.31
+(define (tree-map proc tree)
+  (map (lambda (el)
+         (if (pair? el)
+             (tree-map proc el)
+             
+             (proc el)))
+       tree))
+
+(tree-map (lambda (x) (* x x))
+           (list 1
+                 (list 2 (list 3 4) 5)
+                 (list 6 7)))
+
+;2.32
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x)
+                            (cons (car s) x))
+                          rest)))))
+
+(subsets (list 1 2 3))
+
+;2.37
+(define (accumulate op init seq)
+  (if (null? seq)
+      init
+      (op (car seq)
+          (accumulate op init (cdr seq)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map (lambda (x)
+                                       (car x)) seqs))
+            (accumulate-n op init (map (lambda (x)
+                                         (cdr x)) seqs)))))
+
+(accumulate-n +
+              0
+              (list (list 1 2 3)
+                    (list 4 5 6)
+                    (list 7 8 9)
+                    (list 10 11 12)))
+
+
+  
           
