2f5d7e44070608de24035fc7eff6678fb936ba37
Hank Jackson <hjackson277@gmail.com>
2023-01-18 22:06:03 -0500

    cont w5 hardmode

--- 61a/homework_week5.scm
+++ 61a/homework_week5.scm
@@ -169,7 +169,84 @@
 (equal? '(this is a list)'(this '(is a) list))
 
 
+;2.67
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
 
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (left-branch-h tree) (car tree))
+
+(define (right-branch-h tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch-h branch))
+        ((= bit 1) (right-branch-h branch))
+        (else (error "bad bit"))))
+
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(decode sample-message sample-tree)
+
+;2.68
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) '())
+        ((member symbol (symbols (left-branch-h tree)))
+         (cons 0
+               (encode-symbol symbol (left-branch-h tree))))
+        ((member symbol (symbols (right-branch-h tree)))
+         (cons 1
+               (encode-symbol symbol (right-branch-h tree))))))
+
+(encode '(A D A B B C A) sample-tree)
 
   
           
