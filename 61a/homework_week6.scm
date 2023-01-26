d0acbb00fc2cd836d5eb9b347699c6e117e97311
Hank Jackson <hjackson277@gmail.com>
2023-01-26 16:50:19 -0500

    cont hw week 6

--- 61a/homework_week6.scm
+++ 61a/homework_week6.scm
@@ -8,36 +8,37 @@
 
 ;;A.
 ;we are assuming each div file has a type tag with name
-;exp of div record: ('name-of-div (('bill (records)) ('jane (records)) ('jamie (records))))
+;exp of div record:
+'('name-of-div (('bill (records)) ('jane (records)) ('jamie (records))))
 
 ;dept install:
-;(define (install-research-div)
-;   (define (get-records emp-name file).......)
-;   (put 'get-record 'research get-record)
+(define (install-research-div)
+   (define (get-records emp-name file).......)
+   (put 'get-record 'research get-record))
 
 ;HQ get:
-;(define (get-record employee file)
-;  (attach-tag (type-tag file)
-;              ((get 'get-record (type-tage file)) employee (contents file)))
+(define (get-record employee file)
+  (attach-tag (type-tag file)
+              ((get 'get-record (type-tage file)) employee (contents file))))
 
 ;;B.
 ;dept install:
-;(define (install-research-div)
-;  (define (get-salary record)....)
-;  (put 'get-salaray 'reseach get-salary)
+(define (install-research-div)
+  (define (get-salary record)....)
+  (put 'get-salaray 'reseach get-salary))
 
 ;HQ get:
 ;(define (get-salary employee-record)
 ;  ((get 'get-salaray (type-tage employee-record)) (contents employee-record)))
 
 ;;C.
-;(define (find-employee-record emp-name div-files)
-;  (if (null? div-files)
-;      #f
-;      (let ((emp-records (get-records emp-name (car div-files)))
-;        (if (contents emp-records)
-;             emp-records
-;            (find-employee-records emp-name (cdr div-files)))))
+(define (find-employee-record emp-name div-files)
+  (if (null? div-files)
+      #f
+      (let ((emp-records (get-records emp-name (car div-files)))
+        (if (contents emp-records)
+             emp-records
+            (find-employee-records emp-name (cdr div-files)))))))
 
 
 ;;D.
@@ -56,3 +57,53 @@
   dispatch)
 
 ;2.76
+
+;;Explicit dispatch -- requires modifiying the procedure definitions themseleves to add new cases depending on the tag name.
+;;Data-directed -- requires putting new procedues into table to later get based on type and functionality.
+;;Message-passing -- requires creating new data objects as each new data type is constructor made.
+
+;;message-passing is better for adding new types because you can define the type with its associated functionality discretely.
+;;Data-directed is better for adding new operations because you simply add the operations for each type to the table.
+
+;2.77
+;without the put operations listed in the book for this exersize, there is no reference to what type of complex number
+;we are looking for (or reference to any magnitude procedure). Once we add said put procs, when magnitude is applied to the
+;object, we can check the "sub" type tag, see that it is rectange, and call both real-part and imag-part to calculate the
+;magnitude, as per the installed procedures are defined for a rectangle type. When apply-generic is called on type complex number,
+;with 'magnitude, it will lookup, and apply magnitude to the 'contents' of the object, which is the rectangle obj.
+;
+;(define (magnitude z) (apply-generic 'magnitude z))
+;
+; (magnitude obj: complex) -> (magnitude sub-obj: rectange) -> res
+; 2 calls to apply generic total.
+
+;2.79
+(define (equ? x y) (apply-generic 'equ? x y))
+
+;install to ordinary
+(define (equ-ord? x y) (= x y))
+
+(put 'equ? '(scheme-number scheme-numer)
+     (lambda (x y) (tag (equ-ord? x y))))
+
+;install to rat
+(define (equ-rat? x y)
+  (and (= (* (denom x)(numer y)))
+       (= (* (numer x)(denom y)))))
+  
+(put 'equ? '(rational rational)
+     (lambda (x y) (tag (equ-rat? x y))))
+
+;install to complex
+(define (equ-complex? x y)
+  (and (= (real-part x)(real-part y))
+       (= (imag-part x)(image-part y))))
+
+(put 'equ? '(complex complex)
+     (lambda (x y) (tag (equ-complex x y)))) 
+
+;2.80
+
+;2.81
+
+;2.83
