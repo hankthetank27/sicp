87ce15d092f059391fa02baa83a925ed001b1ebe
Hank Jackson <hjackson277@gmail.com>
2023-02-17 14:07:48 -0500

    completed lab w 9

--- 61a/lab_week9.scm
+++ 61a/lab_week9.scm
@@ -7,13 +7,13 @@
       x
       (cons (car x) (append (cdr x) y))))
 
-(define (find-last x)
+(define (last-pair x)
   (if (null? (cdr x))
       x
-      (find-last (cdr x))))
+      (last-pair (cdr x))))
 
 (define (append! x y)
-  (set-cdr! (find-last x) y)
+  (set-cdr! (last-pair x) y)
   x)
 
 (define x '(a b))
@@ -31,33 +31,52 @@
 ;A CS 61A student, intending to change the value of x to a pair with car equal to 1 and cdr equal to 2,
 ;types the expression (set! (cdr x) y) instead of (set-cdr! x y) and gets an error. Explain why.
 
+;because (cdr x) returns a pair, and set! changes bindings, not pointers in compound data types
 
 ;3a.
 ;Provide the arguments for the two set-cdr! operations in the blanks below to produce the indicated
 ;effect on list1 and list2. Do not create any new pairs; just rearrange the pointers to the existing ones.
-;> (define list1 (list (list ’a) ’b))
-;list1
-;> (define list2 (list (list ’x) ’y))
-;list2
+(define list1 (list (list 'a) 'b))
+(define list2 (list (list 'x) 'y))
 ;> (set-cdr! ________ ________ )
-;okay
+(set-cdr! (car list2) (cdr list1))
 ;> (set-cdr! ________ ________ )
-;okay
-;> list1
-;((a x b) b)
-;> list2
-;((x b) y)
+(set-cdr! (car list1) (car list2))
+list1 ;((a x b) b)
+list2 ;((x b) y)
 
 ;3b.
 ;After filling in the blanks in the code above and producing the specified effect on list1 and list2,
 ;draw a box-and-pointer diagram that explains the effect of evaluating the expression
-;(set-car! (cdr list1) (cadr list2))
-
+(set-car! (cdr list1) (cadr list2))
+list1 ;((a x y) y)
 
 ;;4
-
-
 ;3.13
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define _z (make-cycle '(a b c)))
+
+; (last-pair _z) causes infinte recursion. the cdr of the last pair of the input to make-cycle in the
+; definition of _z points to the last pair to the input, making it a cyclical list.
 
 
 ;3.14
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+; mystery will reduce in the input list to its first element, and return a reversed copy of the input.
+
+(define v '(a b c d))
+(define u (mystery v))
+v
+u
+
+
