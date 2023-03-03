d9fc2110169155d6dc35e3b59d31250d42535fa1
Hank Jackson <hjackson277@gmail.com>
2023-03-02 20:31:43 -0500

    cont hw week 9

--- 61a/homework_week9.scm
+++ 61a/homework_week9.scm
@@ -7,3 +7,28 @@
 
 
 ;3.16
+;;drawn in notebook
+
+;3.17
+;wrote and used before discovering memq
+#;(define (includes? target list)
+  (cond ((null? list) #f)
+        ((eq? target list) #t)
+        (else (includes? target (cdr list)))))
+
+(define (count-pairs x)
+  (let ((seen '()))
+    (define (count x)
+      (cond ((memq x seen) 0)
+            ((not (pair? x)) 0)
+            (else
+             (begin (set! seen (cons seen x))
+                    (+ (count (car x))
+                       (count (cdr x))
+                       1)))))
+    (count x)))
+
+
+
+
+                            
