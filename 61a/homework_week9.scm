b3f9124fc5fceaad9d701503255233c123b724b7
Hank Jackson <hjackson277@gmail.com>
2023-03-05 18:46:28 -0500

    cont hw week 9

--- 61a/homework_week9.scm
+++ 61a/homework_week9.scm
@@ -21,9 +21,9 @@
 ;3.17
 ;wrote and used before discovering memq
 #;(define (includes? target list)
-  (cond ((null? list) #f)
-        ((eq? target list) #t)
-        (else (includes? target (cdr list)))))
+    (cond ((null? list) #f)
+          ((eq? target list) #t)
+          (else (includes? target (cdr list)))))
 
 (define (count-pairs x)
   (let ((seen '()))
@@ -72,8 +72,87 @@
 ;drawn in notebook
 
 ;3.21
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE called with empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+(define (print-queue queue)
+  (display (front-ptr queue)))
+
+(define q1 (make-queue))
+(insert-queue! q1 'a)
+(insert-queue! q1 'b)
+(delete-queue! q1)
+(delete-queue! q1)
+
+
+;3.22
+(define (make-q)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+    (define (set-front-ptr! item) (set! front-ptr item))
+    (define (set-rear-ptr! item) (set! rear-ptr item))
+    (define (empty-queue?) (null? front-ptr))
+    (define (dispatch m)
+      (cond ((eq? m 'front-queue)
+             (lambda ()
+               (if (empty-queue?)
+                   (error "FRONT called with empty queue"))))
+            ((eq? m 'insert-queue!)
+             (lambda (item)
+               (let ((new-pair (cons item '())))
+                 (cond ((empty-queue?)
+                        (set-front-ptr! new-pair)
+                        (set-rear-ptr! new-pair)
+                        front-ptr)
+                       (else
+                        (set-cdr! rear-ptr new-pair)
+                        (set-rear-ptr! new-pair)
+                        front-ptr)))))
+            ((eq? m 'delete-queue!)
+             (lambda ()
+               (cond ((empty-queue?)
+                      (error "DELETE called with empty queue"))
+                     (else
+                      (set-front-ptr! (cdr front-ptr))
+                      front-ptr))))
+            (else (error "Undefined operation -- QUEUE" m))))
+    dispatch))
+
+(define q2 (make-q))
+((q2 'insert-queue!) 'a)
+((q2 'insert-queue!) 'b)
+((q2 'insert-queue!) 'c)
+((q2 'delete-queue!))
+((q2 'delete-queue!))
+((q2 'delete-queue!))
 
 
 
 
-                            
