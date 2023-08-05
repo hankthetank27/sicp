993348d8ce93310c84b3132fcbf0e4189acd4fbd
Hank Jackson <hjackson277@gmail.com>
2023-08-04 22:08:37 -0400

    started hw week 10

---
+++ 61a/homework_week10.scm
@@ -0,0 +1,36 @@
+#lang sicp
+(load "parallel-execute-and-serializer")
+
+; 3.38
+
+;; a. 35, 40, 45, 50
+;; b. It could be any single transation, ie. 110, 80, 50, or any combination of two transactions, ie. 90, 40, 
+
+; 3.39 121, 100, 101, 11
+
+; 3.40
+
+; 3.41
+
+; 3.42
+
+; 3.44
+
+; 3.46
+
+; 3.48
+
+
+; -------------------------------
+; EXTRA FOR EXPERTS
+; read 3.3.5
+
+; 3.33
+
+; 3.34
+
+; 3.35
+
+; 3.36
+
+; 3.37
